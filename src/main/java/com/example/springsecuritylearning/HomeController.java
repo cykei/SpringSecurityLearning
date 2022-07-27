827223ccc7d3c47106bd8f637a7342a7e4e8d15e
cykei <kelle111@naver.com>
2022-07-28 00:38:22 +0900

    그룹 멤버 목록 가져오기

--- src/main/java/com/example/springsecuritylearning/HomeController.java
+++ src/main/java/com/example/springsecuritylearning/HomeController.java
@@ -1,16 +1,25 @@
 package com.example.springsecuritylearning;
 
 import lombok.RequiredArgsConstructor;
+
 import javax.naming.NamingException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.naming.directory.Attributes;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
@@ -18,28 +27,64 @@ public class HomeController {
     private final LdapTemplate ldapTemplate;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "hello world!";
     }
 
     @GetMapping("/design")
-    public String design() {return "design";}
+    public String design() {
+        return "design";
+    }
 
     @GetMapping("/search")
     public List<String> search() {
-        return ldapTemplate.search(
-                query().where("objectclass").is("person"),
-                new AttributesMapper<String>() {
-                    public String mapFromAttributes(Attributes attrs)
+
+        AndFilter filter = new AndFilter();
+        filter.and(new EqualsFilter("objectclass", "groupOfUniqueNames"));
+        filter.and(new EqualsFilter("cn", "developers"));
+
+        List<Object> results = ldapTemplate.search(
+                "", filter.encode(),
+                new AttributesMapper() {
+                    public Object mapFromAttributes(Attributes attrs)
                             throws NamingException {
-                        return (String) attrs.get("cn").get();
+                        List aa = Collections.list(attrs.get("uniqueMember").getAll());
+                        for (Object a : aa) {
+                            log.info("a: " + (String) a);
+                        }
+                        return aa;
                     }
                 });
+
+        ArrayList<String> arrayList = (ArrayList<String>) results.get(0);
+        return arrayList;
+
+        /* 아래 코드는 ben 하나만 가져옴 */
+//       List<String> results =  ldapTemplate.search(
+//                "", filter.encode(),
+//                new AttributesMapper() {
+//                    public Object mapFromAttributes(Attributes attrs)
+//                            throws NamingException {
+//                        return attrs.get("uniqueMember").get();
+//                    }
+//                });
+
+        /* 아래 코드는 모든 groupOfUniqueNames 에서 가장 위에 있는 uniqueMember만 출력함. */
+
+//        return ldapTemplate.search(
+//                query().where("objectclass").is("groupOfUniqueNames"),
+//                new AttributesMapper() {
+//                    public String mapFromAttributes(Attributes attrs)
+//                            throws NamingException {
+//                        return (String) attrs.get("uniqueMember").get();
+//                    }
+//                });
 
         /*
         결과
 
-        ["quote\\\"guy","Joe Smeth","Mouse, Jerry","slash/guy","Ben Alex","Bob Hamilton","Space Cadet"]
-         */
+       ["uid=ben,ou=people,dc=springframework,dc=org","uid=ben,ou=people,dc=springframework,dc=org","uid=ben,ou=people,dc=springframework,dc=org"]
+
+        */
     }
 }
